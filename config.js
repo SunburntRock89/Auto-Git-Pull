384fc1f24674b5df14967d4fb3c2bd741cd92a84
SunburntRock89 <SunburntRock89@gmail.com>
2018-05-05 00:38:05 +0100

    implement a fix

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	repos: [
-		{ name: "repo-name", location: "/direct/path/to/repo", logRes: true },
+		// eslint-disable-next-line no-inline-comments
+		{ name: "repo-owner/repo-name", location: "/direct/path/to/repo", logRes: true, host: "github" /* Can also be GitLab */ },
 	],
 	port: 8080,
 };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = new (require("express"))();
 const config = require("./config");
 
 app.post("/", async(req, res) => {
-	let repo = config.repos.find(r => r.name.toLowerCase() === req.body.repository.name.toLowerCase());
+	let repo = config.repos.find(r => r.name.toLowerCase() === `${r.host == "gitlab" ? req.body.project.path_with_namespace.toLowerCase() : req.body.repository.full_name.toLowerCase()}`);
 	if (!repo) return;
 	require("fs").readDirSync(repo.location).catch(() => console.error(`Warning, repository "${repo.name} has an invalid location."`));
 	let result = await exec("git pull", { cwd: repo.location, timeout: 100000 }).catch(() => console.err(`Failed to pull in repository: ${repo.name}`));
